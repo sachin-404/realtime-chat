3b2b1d48413e090960b6fa7d6d883bb5809c45f2
Sachin Kant <sachinkant404@gmail.com>
2023-12-23 20:17:23 +0530

    created ui for listing chat rooms and chat box

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('rooms/<slug:slug>/', views.room, name='room'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import SignUpForm
+from .models import Room
 
 # Create your views here.
 
@@ -25,3 +27,16 @@ def signup(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    
+    return render(request, 'core/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    
+    return render(request, 'core/room.html', {'room': room})
