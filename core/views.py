adba9974dc2570a7a184892a57f25c094dc3685e
Sachin Kant <sachinkant404@gmail.com>
2023-12-23 15:36:31 +0530

    implemented user auth (signup,login, logout)

--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+
+from .forms import SignUpForm
 
 # Create your views here.
 
 def home(request):
     return render(request, "core/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'core/signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
