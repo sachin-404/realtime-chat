ca5030102ac8f2427d88c4da6f7ef5b734c30059
Sachin Kant <sachinkant404@gmail.com>
2023-12-28 13:20:37 +0530

    update settings

--- chat_room/settings.py
+++ chat_room/settings.py
@@ -141,7 +141,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
